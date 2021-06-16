db029e3b5df9a5009434b739dbb21a4eacac72bb
ddzin <86007410+ddzin-dev@users.noreply.github.com>
2021-06-16 11:49:53 -0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,40 @@
+/* 
+
+
+Handler Básico oficial na documentação do Discord.js (Apenas para Testes);
+
+
+*/
+
+const Discord = require("discord.js");
+const Enmap = require("enmap");
+const fs = require("fs");
+const client = new Discord.Client();
+const config = require("./config.json");
+client.config = config;
+
+fs.readdir("./events/", (err, files) => {
+  if (err) return console.error(err);
+  files.forEach(file => {
+    const event = require(`./events/${file}`);
+    let eventName = file.split(".")[0];
+    client.on(eventName, event.bind(null, client));
+  });
+});
+
+client.commands = new Enmap();
+
+fs.readdir("./commands/", (err, files) => {
+  if (err) return console.error(err);
+  files.forEach(file => {
+    if (!file.endsWith(".js")) return;
+    let props = require(`./commands/${file}`);
+    let commandName = file.split(".")[0];
+    console.log(`${commandName} carregado.`);
+    client.commands.set(commandName, props);
+  });
+});
+
+
+// iniciar o bot:
+client.login(config.token); 
