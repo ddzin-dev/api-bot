d0b04ad3dc9019c6aaed15d31656a3d877690a90
ddzin <86007410+ddzin-dev@users.noreply.github.com>
2021-06-16 12:34:50 -0300

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -4,5 +4,21 @@ module.exports = async client => {
 
   client.user.setActivity('Cachorrão do ddzin :)', {type: 'PLAYING'});
   console.log('online');
+  
+var requestOptions = {
+  method: 'GET/POST',
+  headers: {
+    "authorization": "AUTH_TOKEN",
+    "Content-Type": "application/json"
+  },
+  body: JSON.stringify({"server_count": 1500}); 
+};
+
+fetch(URL, requestOptions)
+  .then(response => response.text())
+  .then(console.log)
+  .catch(console.error);
 
 }
+
+/* NÃO UTILIZE ESTE MODELO AINDA! */
